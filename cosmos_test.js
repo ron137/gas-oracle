fced39d0a5982563b7469e58133b3d9c602ec465
werlang <pswerlang@gmail.com>
2022-05-14 17:27:54 +0000

    Finish cosmos_test
    * Add args to control sampleSize
    * getBlock return block in the same format the original oracle.
    * Add calcBlockStats and recordBlocks.

--- cosmos_test.js
+++ cosmos_test.js
@@ -1,7 +1,24 @@
 const fetch = require('node-fetch');
+const fs = require('fs');
 
-// TODO
-// need to pick gasList and build file from it.
+const args = {
+    sampleSize: 1000,
+    timeInterval: 5000,
+};
+
+// receive args
+process.argv.forEach((val, index, array) => {
+    if ((val == '-s' || val == '--sample-size') && array[index+1]){
+        args.sampleSize = array[index+1];
+    }
+    if ((val == '-t' || val == '--time-interval') && array[index+1]){
+        args.timeInterval = array[index+1];
+    }
+});
+
+let lastBlock = 0;
+let timeInterval = args.timeInterval;
+const blocks = {};
 
 (async () => {
     // get latest block number
@@ -9,32 +26,44 @@ const fetch = require('node-fetch');
         const url = 'https://rpc.cosmos.network'
         const res = await fetch(`${url}/block`);
         const block = await res.json();
+        // console.log(block)
         const height = block.result.block.header.height;
 
         return height;
     };
 
     // get a list of gas prices for every tx in the block
-    const getGasList = async block => {
+    const getBlock = async blockNumber => {
         const url = 'https://api.cosmos.network/cosmos/tx/v1beta1';
-        const res = await fetch(`${url}/txs?events=tx.height=${block}`);
+        const res = await fetch(`${url}/txs?events=tx.height=${blockNumber}`);
         const tx = await res.json();
+        // console.log(tx)
         
-        const list = tx.txs.map(e => {
+        const gasList = tx.txs.map(e => {
             try {
                 if (!e.auth_info.fee.amount.length) {
                     return 0;
                 }
-                return e.auth_info.fee.amount[0].amount;
+                return parseFloat(e.auth_info.fee.amount[0].amount);
             }
             catch(error) {
                 console.log(error);
                 console.log(e);
                 return 0;
             }
-        }).filter(e => parseInt(e) > 0);
+        }).filter(e => parseFloat(e) > 0);
 
-        return list;
+        const block = {
+            gasList: gasList,
+            number: blockNumber,
+        };
+
+        if (tx && tx.tx_responses && tx.tx_responses.length) {
+            block.timestamp = parseInt(new Date(tx.tx_responses[0].timestamp).getTime() / 1000);
+            block.avgGas = tx.tx_responses.reduce((p,c) => p + parseFloat(c.gas_used), 0) / tx.tx_responses.length;
+        }
+
+        return block;
     };
 
     // automatically calculate time interval between calls
@@ -63,20 +92,69 @@ const fetch = require('node-fetch');
         return timeInterval;
     };
 
-    let lastBlock = 0;
-    let timeInterval = 5000;
+    const calcBlockStats = () => {
+        // sort blocks by timestamp, then remove blocks with no tx
+        const b = Object.values(blocks).sort((a,b) => a.timestamp - b.timestamp).filter(e => e.ntx);
+
+        // reshape blocks object to be arrays of each field
+        const result = Object.fromEntries(Object.keys(b[0]).map(e => [e, []]));
+        b.forEach(block => Object.keys(result).forEach(key => result[key].push(block[key])));
+
+        // last block
+        const lastBlock = parseInt(Object.keys(blocks).sort((a,b) => parseInt(b) - parseInt(a))[0]);
+        result.lastBlock = lastBlock;
+        // timestamp from last block
+        result.lastTime = blocks[lastBlock].timestamp;
+
+        fs.writeFileSync(`./blockStats_cosmos.json`, JSON.stringify(result));
+        return result;
+    };
+
+    const recordBlocks = async block => {
+        blocks[block.number] = {
+            ntx: block.gasList.length,
+            timestamp: block.timestamp,
+            minGwei: block.gasList.sort((a,b) => a-b),
+            avgGas: block.avgGas,
+        };
+
+        // sort the blocks and discard if higher than sampleSize
+        const sortedBlocks = Object.keys(blocks).sort((a,b) => parseInt(a) - parseInt(b));
+        if (sortedBlocks.length > args.sampleSize){
+            delete blocks[sortedBlocks[0]];
+
+            calcBlockStats();
+            console.log(`${new Date().toISOString()}: New block ${ block.number } read. Next update: ${ timeInterval.toFixed(1) }ms`);
+        }
+        else{
+            // pretty progress bar
+            const barSize = 50;
+            const filledBars = parseInt(sortedBlocks.length / args.sampleSize * barSize);
+            const barString = [...Array(filledBars).fill('#'), ...Array(barSize - filledBars).fill('=')].join('');
+            if (sortedBlocks.length == args.sampleSize){
+                const barString = Array(barSize).fill('#').join('');
+                console.log(`[${barString}] ${sortedBlocks.length} / ${args.sampleSize}`);
+            }
+            else{
+                console.log(`[${barString}] ${sortedBlocks.length} / ${args.sampleSize}`);
+            }
+        }
+
+    };
 
     // loop function
     const loop = async () => {
         const currentBlock = await getBlockHeight();
+        // console.log(currentBlock);
 
         let state = 0;
         if (currentBlock > lastBlock) {
-            const gasList = await getGasList(currentBlock);
+            const block = await getBlock(currentBlock);
+            // console.log(block)
 
-            if (gasList.length) {
-                console.log(`${new Date().toISOString()}: New block ${ currentBlock } read. Next update: ${ timeInterval.toFixed(1) }ms`);
-                // console.log(gasList);
+            if (block) {
+                // console.log(block);
+                recordBlocks(block);
                 state = 1;
             }
 
