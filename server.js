a6360d5f0207136da269a208e47c9dd8c52a6ed3
werlang <pswerlang@gmail.com>
2022-07-06 23:08:37 +0000

    Add old blocks enables flag.

--- server.js
+++ server.js
@@ -68,13 +68,17 @@ app.get('/:network', cors(), (req, res) => {
 
 // collect more history data
 app.get('/:network/blocks', cors(), async (req, res) => {
-    res.status(404);
-    res.send({
-        status: 404,
-        error: 'Not found',
-        message: 'This endpoint was disabled.'
-    });
-    return;
+    const enabled = false;
+
+    if (!enabled) {
+        res.status(404);
+        res.send({
+            status: 404,
+            error: 'Not found',
+            message: 'This endpoint was disabled.'
+        });
+        return;
+    }
 
     const network = req.params.network;
     const fromblock = parseInt(req.query.fromblock);
